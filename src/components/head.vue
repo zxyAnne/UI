d75ca577d9dc11fdec712293d0f6524154e6f779
zxypuoke <zxy@puoke.com>
2018-05-04 15:49:27 +0800

    ui

---
+++ src/components/head.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div class="head-container">
+            dfbdf
+        </div>
+    </header>
+</template>
+<script>
+export default {
+  name: 'pic',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+<style lang="sass" type="text/scss" scoped="">
+.pic{
+    &-title{
+        font-size: 20px;
+    }
+}
+</style>
